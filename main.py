411e42cf51d043c5bc84b1db29fa365fb0ae3548
JeysonBarrios <pruebalandiaia@gmail.com>
2026-04-14 10:53:22 -0500

    Laboratorio completado

---
+++ main.py
@@ -0,0 +1,9 @@
+for number in  range(1,1001):
+    if number % 3 == 0
+       Print("Fizz")
+       elif number % 5== 0:
+        Print("Buzz")
+        elif number % 3 == 0 and number % 5 == 0:
+            Print("FizzBuzz")
+            else:
+                Print(number)
